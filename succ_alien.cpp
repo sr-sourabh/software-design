538e680be3dc2bc53188f866896e4297fa584523
sr-sourabh <sourabhchaudhary012@gmail.com>
2021-08-22 21:52:05 +0530

    Generating bidirectional maps is small new funtion with independent responsibility

--- succ_alien.cpp
+++ succ_alien.cpp
@@ -1,13 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string succ_alien(string num, string alphabets){
-    int base = alphabets.size();
+/*
+Given alphabets of a number system this function returns both way maps.
+i.e alphabet to digit and digit to alphabet maps
+Digits start with 0,1,2 etc for corresponding alphabet positions in alpbhabet string
+Params : alphabets of a number system as string
+Return : pair of {alphabetToDigitMap, digitToAlphabetMap}
+*/
+pair<unordered_map<char, char>, unordered_map<char, char>> getBidirectionalMaps(string alphabets){
     unordered_map<char, char> alphabetToDigitMap, digitToAlphabetMap;
     for(int i = 0 ; i < alphabets.size(); i++){
         alphabetToDigitMap[alphabets[i]] = i + '0';
         digitToAlphabetMap[i+'0'] = alphabets[i];
     }
+    return {alphabetToDigitMap, digitToAlphabetMap};
+}
+
+string succ_alien(string num, string alphabets){
+    int base = alphabets.size();
+    auto bidirectionalMaps = getBidirectionalMaps(alphabets);
+    auto alphabetToDigitMap = bidirectionalMaps.first;
+    auto digitToAlphabetMap = bidirectionalMaps.second;
     string numericNum = "";
     for(int i = 0 ; i < num.size(); i++){
         numericNum += alphabetToDigitMap[num[i]];
